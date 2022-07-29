5dd171b0c3277d956bb36f327b157a73e34468a6
Cachalot792 <83542490+Cachalot792@users.noreply.github.com>
2022-07-29 19:37:44 +0900

    reset

---
+++ README.md
@@ -0,0 +1,4 @@
+# ウェブサイトを作ってみた
+過去2度ほど断念したウェブサイト作りをもう1度チャレンジしてみました。   
+[ホームページリンク](https://cachalot792.github.io/)  
+[記法参考](https://qiita.com/tbpgr/items/989c6badefff69377da7)  
---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.nav-bar').on('click', function() {
+      $('.space').toggleClass('hidden-space');
+      $('nav').toggleClass('hidden-nav');
+      $('.nav-bar').toggleClass('nav-bar-white');
+      $('header').toggleClass('header-onclick');
+    });
+});
+
+$(window).scroll(function(){
+  if($(window).scrollTop() > 80){
+  $('header').addClass('header-scroll');
+  $('header img').addClass('header-scroll-img');
+  $('header h1').addClass('header-scroll-h1');
+  $('header ul').addClass('header-scroll-ul');
+  $('.nav-bar').addClass('header-scroll-nav');
+  }else{
+    $('header').removeClass('header-scroll');
+    $('header img').removeClass('header-scroll-img');
+    $('header h1').removeClass('header-scroll-h1');
+    $('header ul').removeClass('header-scroll-ul');
+    $('.nav-bar').removeClass('header-scroll-nav');
+  }
+});
+
+if (window.matchMedia('(max-width: 800px)').matches) {
+  $('.space').addClass('hidden-space');
+  $('nav').addClass('hidden-nav');
+  $('.nav-bar').addClass('nav-bar-white');
+  $('header').addClass('header-onclick');
+};
